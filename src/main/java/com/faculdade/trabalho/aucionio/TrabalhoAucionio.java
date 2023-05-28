f02b03a72cd86fd8a3976fafc014570e7a84a241
Isaac-alencar <alencarisaac3@gmail.com>
2023-05-28 13:08:19 -0300

    trabalho faculdade

---
+++ src/main/java/com/faculdade/trabalho/aucionio/TrabalhoAucionio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.faculdade.trabalho.aucionio;
+
+/**
+ *
+ * @author isaac
+ */
+public class TrabalhoAucionio {
+
+    public static void main(String[] args) {
+        System.out.println("Cu do professor!");
+    }
+}
